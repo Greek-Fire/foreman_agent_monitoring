341440a67f15755dc98271ad6bfdb741741d3ee4
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 09:53:49 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ForemanAgentMonitoring::Engine.routes.draw do
-  get 'new_action', to: 'example#new_action', as: 'new_action'
-  get 'plugin_template_description', to: 'example#react_template_page_description'
-  get 'welcome', to: '/react#index', as: 'welcome'
+  get 'new_action', to: 'agents#new_action', as: 'new_action'
+  get 'plugin_template_description', to: 'agents#react_template_page_description'
+  get 'welcome', to: 'react#index', as: 'welcome'
 end
 
 Foreman::Application.routes.draw do
