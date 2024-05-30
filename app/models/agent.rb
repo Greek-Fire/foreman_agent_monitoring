a86d3a425474f8eb3846d8b3b2d901c80b717f74
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 17:21:54 -0400

    push

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,5 +1,4 @@
 class Agent < ApplicationRecord
-  include Authorizable
   scoped_search on: :network, complete_value: true
   scoped_search on: :status, complete_value: true
   scoped_search on: :agent, complete_value: true
