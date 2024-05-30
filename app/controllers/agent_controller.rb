6fee9053a4a8ece26d627c7d33b89e02d7db6ffd
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 09:40:11 -0400

    push

---
+++ app/controllers/agent_controller.rb
@@ -0,0 +1,24 @@
+module ForemanAgentMonitoring
+    class AgentController < ApplicationController
+      def index
+        # Your code here
+      end
+  
+      def show
+        # Your code here
+      end
+  
+      def edit
+        # Your code here
+      end
+  
+      def update
+        # Your code here
+      end
+  
+      def destroy
+        # Your code here
+      end
+    end
+  end
+  
