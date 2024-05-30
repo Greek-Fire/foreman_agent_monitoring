ecfeb7f0ed722ad3feca674c242e3ca8572e80b8
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 15:44:48 -0400

    push

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,8 +1,7 @@
-module ForemanAgentMonitoring
-  class Agent < ApplicationRecord
-    scoped_search on: :network, complete_value: true
-    scoped_search on: :status, complete_value: true
-    scoped_search on: :agent, complete_value: true
-    scoped_search on: :host
-  end
+class Agent < ApplicationRecord
+  include Authorizable
+  scoped_search on: :network, complete_value: true
+  scoped_search on: :status, complete_value: true
+  scoped_search on: :agent, complete_value: true
+  scoped_search on: :host
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
-ForemanAgentMonitoring::Engine.routes.draw do
+Rails.application.routes.draw do
+
   resources :agents do
     get :results, on: :collection
     get :auto_complete_search, on: :collection
   end
-
-  root to: 'agents#index'
-end
-
-Foreman::Application.routes.draw do
-  mount ForemanAgentMonitoring::Engine, at: '/foreman_agent_monitoring'
 end
