f7cd2dac73efd8078597a2ac5e5d47d722872840
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 10:07:32 -0400

    push

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,7 +1,8 @@
-class Agent < ApplicationRecord
-  scoped_search on: :network, complete_value: true
-  scoped_search on: :status, complete_value: true
-  scoped_search on: :agent, complete_value: true
-  scoped_search on: :host
-  
+module ForemanAgentMonitoring
+  class Agent < ApplicationRecord
+    scoped_search on: :network, complete_value: true
+    scoped_search on: :status, complete_value: true
+    scoped_search on: :agent, complete_value: true
+    scoped_search on: :host
+  end
 end
