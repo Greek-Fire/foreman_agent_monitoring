b9619ccbddd9f9452ff7ec38c9ffd8158c7d8048
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 17:52:50 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+
 ForemanAgentMonitoring::Engine.routes.draw do
-  get 'new_action', to: 'example#new_action', as: 'new_action'
-  get 'plugin_template_description', to: 'example#react_template_page_description'
-  get 'welcome', to: '/react#index', as: 'welcome'
+  resources :agents do
+    get :auto_complete_search, on: :collection
+    get 'welcome', to: '/react#index', as: 'welcome'
+  end
 end
 
 Foreman::Application.routes.draw do
-  mount ForemanAgentMonitoring::Engine, at: '/foreman_agent_monitoring'
+  mount ForemanAgentMonitoring::Engine, at: '/Foreman_agent_monitoring'
 end
