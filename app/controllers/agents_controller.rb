f156689d867272b59fa9adedb589447076093807
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 16:00:34 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -2,8 +2,6 @@ class AgentsController < ApplicationController
   include Foreman::Controller::AutoCompleteSearch
   include Foreman::Controller::Parameters::Agent
   
-  before_action :find_resource, :only => [:edit, :update, :destroy]
-
   def index
     @agents = resource_base_search_and_page
   end
