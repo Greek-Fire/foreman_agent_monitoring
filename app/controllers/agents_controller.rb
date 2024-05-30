44ffd2c45d62890437a1cfc27aca8f2d53f624a8
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 10:41:19 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,5 +1,6 @@
 class AgentsController < ApplicationController
   include Foreman::Controller::AutoCompleteSearch
+  include Foreman::Controller::Parameters::Agent
 
   def index
     @agents = resource_base_search_and_page
