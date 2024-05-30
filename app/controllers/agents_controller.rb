701de29867e6b2bd4c0ff5c530c9382bcd88d35e
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 17:24:20 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -3,9 +3,6 @@ class AgentsController < ApplicationController
   include Foreman::Controller::Parameters::Agent
   
   def index
-    @agents = resource_base_search_and_page
-
-    respond_to do |format|
-      format.html # index.html.erb
+    @agents
     end
 end
