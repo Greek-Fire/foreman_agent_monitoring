2613ee660f92e94dcc19022538d9f9de206287c4
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 18:16:24 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 
-ForemanAgentMonitoring::Engine.routes.draw do
+Rails.application.routes.draw do
   resources :agents do
     get :auto_complete_search, on: :collection
   end
-end
-
-Foreman::Application.routes.draw do
-  mount ForemanAgentMonitoring::Engine, at: '/Foreman_agent_monitoring'
 end
