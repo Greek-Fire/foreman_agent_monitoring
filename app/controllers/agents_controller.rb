7c0fde46abe38e07b8cd5ecbd0c6946a181f8521
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 20:55:05 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,13 +1,16 @@
-class AgentsController < ApplicationController
-  include Foreman::Controller::AutoCompleteSearch
-  include Foreman::Controller::Parameters::Agent
-  
-  def index
-    @agents = resource_base_search_and_page
-  end
+module ForemanAgentMonitoring
 
-  def show
-    # Display individual build report.
-  end
+  class AgentsController < ApplicationController
+    include Foreman::Controller::AutoCompleteSearch
+    include Foreman::Controller::Parameters::Agent
+    
+    def index
+      @agents = resource_base_search_and_page
+    end
 
+    def show
+      # Display individual build report.
+    end
+
+  end
 end
