733025100366a2ac4d540955c603c56a1c6d4276
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-31 11:19:49 -0400

    push

--- db/migrate/20240529_create_agents.rb
+++ db/migrate/20240529_create_agents.rb
@@ -1,17 +1,17 @@
 class CreateAgents < ActiveRecord::Migration[6.0]
-    def change
-      create_table :agents do |t|
-        t.string :network
-        t.string :status
-        t.string :agent
-        t.string :host
-  
-        t.timestamps
-      end
-  
-      add_index :agents, :network
-      add_index :agents, :status
-      add_index :agents, :agent
-      add_index :agents, :host
+  def change
+    create_table :agents do |t|
+      t.boolean :network  # Changed from string to boolean
+      t.string :status
+      t.string :agent
+      t.string :host
+
+      t.timestamps
     end
-  end  
+
+    add_index :agents, :network
+    add_index :agents, :status
+    add_index :agents, :agent
+    add_index :agents, :host
+  end
+end
