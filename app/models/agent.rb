47966d880ff6fd14c5e6f9a4d6926835f0e1d975
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-30 22:01:26 -0400

    push

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,5 +2,5 @@ class Agent < ApplicationRecord
   scoped_search on: :network, complete_value: true
   scoped_search on: :status, complete_value: true
   scoped_search on: :agent, complete_value: true
-  scoped_search on: :host
+  scoped_search on: :host, complete_value: true
 end
